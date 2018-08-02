89bf5dcca15f3759541b843fb2a4a29c11dbc353
surya1singh <surya1.singh@gmail.com>
2018-08-02 16:22:18 +0530

    initialize method for logger

--- logging/log.py
+++ logging/log.py
@@ -1,8 +1,30 @@
+import inspect
 import logging
+import os
+import traceback
 
-class Log_me ():
+MaxBytes = 1024*1024
+BackupCount = 10
+
+class Log_me():
     def __init__(self,**kwargs):
-        pass
+        self.log_dir = kargs["log_dir"]
+        self.filename = kargs["log_file"]
+        self.log_filename = self.log_dir + '/' + self.filename
+        self.frame, self.filename,self.line_number,self.function_name,self.lines,self.index = inspect.getouterframes(inspect.currentframe())[1]
+        if not os.path.isdir(self.log_dir):
+            os.makedirs(self.log_dir)
+            os.chmod(self.log_dir,0777)
+        self.logger = logging.getLogger(self.filename)
+        self.logger.setLevel(logging.DEBUG)
+        self.addHandler()
+
+    def addHandler(self):
+        """ add handler """
+        self.handler = logging.handlers.RotatingFileHandler(self.log_filename, maxBytes=MaxBytes, backupCount=BackupCount)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - (%(threadName)-10s) - %(levelname)s - %(message)s",datefmt='%Y-%m-%d %H:%M:%S')
+        self.handler.setFormatter(formatter)
+        self.logger.addHandler(self.handler)
 
     def info(self,log_data):
         """ info """
