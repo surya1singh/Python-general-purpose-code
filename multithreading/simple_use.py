a44008fedac2e34ffba631bb63670a6bf6dff736
surya1singh <surya1.singh@gmail.com>
2018-08-02 22:57:17 +0530

    Extending with Thread class

--- multithreading/simple_use.py
+++ multithreading/simple_use.py
@@ -1,10 +1,36 @@
 from threading import Thread
 import time
 
-first = Thread(target=print, args=("This is print statement is with input :",1))
-second = Thread(target=print, args=("This is print statement is with input :",2))
-third = Thread(target=print, args=("This is print statement is with input :",3))
-print(first.getName())
-first.start()
-second.start()
-third.start()
+def first_threads():
+    first = Thread(target=print, args=("This is print statement is with input :",1))
+    second = Thread(target=print, args=("This is print statement is with input :",2))
+    third = Thread(target=print, args=("This is print statement is with input :",3))
+    print(first.getName()) # prints name of the thread
+    first.start()
+    second.start()
+    third.start()
+
+first_threads() # create three threads
+
+
+
+class myThread(Thread):
+   def __init__(self):
+      super(myThread, self).__init__()
+
+   def run(self):
+      print("Starting" , self.getName())
+      time.sleep(1)
+      print("Exiting" , self.getName())
+
+
+
+# Create new threads
+thread1 = myThread()
+thread2 = myThread()
+
+# Start new Threads
+thread1.start()
+thread2.start()
+
+print("Exiting Main Thread")
