7dcad4f463064461fc400463dc7b9e796d90d8e3
surya1singh <surya1.singh@gmail.com>
2018-08-02 17:15:21 +0530

    use same logger with multiple thread

--- logging/use_log.py
+++ logging/use_log.py
@@ -1,15 +1,34 @@
 from log import Log_me
+from threading import Thread
+import time
 
+logger = Log_me(log_file='test_logging.log', log_dir='.')
 
 def basicUse():
-    logger = Log_me(log_file='test_logging.log', log_dir='.')
     logger.debug('This is debug info')
+    time.sleep(0.1)
     logger.info('This is info info')
+    time.sleep(0.1)
     logger.warn('This is warning info')
+    time.sleep(0.1)
     logger.error('This is error info')
+    time.sleep(0.1)
     logger.critical('This is critical info')
 
+
+def multithreadUse():
+    t1 = Thread(target=basicUse)
+    t2 = Thread(target=basicUse)
+
+    # starting thread 1
+    t1.start()
+    # starting thread 2
+    t2.start()
+
+    t1.join()
+    t2.join()
 
 
 if __name__ == '__main__':
     basicUse()
+    multithreadUse()
