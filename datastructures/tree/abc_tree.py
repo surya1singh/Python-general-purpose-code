f86d2ab904022995744204399bb6d67c1a4233d6
surya1singh <surya1.singh@gmail.com>
2018-08-11 17:23:47 +0530

    Abstract base class for BinaryTree

--- datastructures/tree/abc_tree.py
+++ datastructures/tree/abc_tree.py
@@ -66,3 +66,38 @@ class Tree():
             return 0
         else:
             return 1 + max(height(c) for c in self.children(p))
+
+
+class BinaryTree(Tree):
+    """Abstract base class representing a binary tree structure."""
+    # --------------------- additional abstract methods ---------------------
+    def left(self, p):
+        """Return a Position representing p's left child.
+        Return None if p does not have a left child.
+        """
+        raise NotImplementedError( must be implemented by subclass )
+
+    def right(self, p):
+        """Return a Position representing p's right child.
+        Return None if p does not have a right child.
+        """
+        raise NotImplementedError( must be implemented by subclass )
+
+    # ---------- concrete methods implemented in this class ----------
+    def sibling(self, p):
+        """Return a Position representing p's sibling (or None if no sibling)."""
+        parent = self.parent(p)
+        if parent is None:
+            return None else:
+        if p == self.left(parent):
+            return self.right(parent)
+        else:
+            return self.left(parent)
+
+    # p must be the root # root has no sibling
+    def children(self, p):
+        """Generate an iteration of Positions representing p's children."""
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
